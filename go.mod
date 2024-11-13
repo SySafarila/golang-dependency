4083898c2dc74b181f5a5321e190eafa64c87f67
Syahrul Safarila <sysafarila.official@gmail.com>
2024-11-13 19:41:05 +0700

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/SySafarila/golang-dependency
+
+go 1.23.3
+
+require github.com/SySafarila/golang-test-module v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	golang_test_module "github.com/SySafarila/golang-test-module"
+)
+
+func main() {
+	fmt.Println(golang_test_module.SayHello())
+}
