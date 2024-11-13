e0454f3b6ba2a4032402cc50f34c0350ea4ffa52
Syahrul Safarila <sysafarila.official@gmail.com>
2024-11-13 19:53:38 +0700

    upgrade dependency

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/SySafarila/golang-dependency
 
 go 1.23.3
 
-require github.com/SySafarila/golang-test-module v1.0.0 // indirect
+require github.com/SySafarila/golang-test-module/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	golang_test_module "github.com/SySafarila/golang-test-module"
+	golang_test_module "github.com/SySafarila/golang-test-module/v2"
 )
 
 func main() {
-	fmt.Println(golang_test_module.SayHello())
+	fmt.Println(golang_test_module.SayHello("Syahrul"))
 }
